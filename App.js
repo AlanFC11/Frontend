76ba6d53d37e38453f6659abea917b175db443d8
AlanFC11 <73494793+AlanFC11@users.noreply.github.com>
2021-04-22 23:23:53 -0500

    Inicial

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import  { NavigationContainer }  from '@react-navigation/native';
+import  { createStackNavigator }  from '@react-navigation/stack';
+import Intro1Screen from './components/intro/Intro1';
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName = "Menu">
+          <Stack.Screen name = "Intro1" component={Intro1Screen} options={{headerShown:false}} />
+          <Stack.Screen name = "Intro2" component={Intro2Screen} options={{headerShown:false}} />
+          <Stack.Screen name = "Intro3" component={Intro3Screen} options={{headerShown:false}} />
+          <Stack.Screen name = "Login" component={LoginScreen} options={{headerShown:false}} />
+          <Stack.Screen name = "Register" component={RegisterScreen}/>
+          <Stack.Screen name = "Menu" component={MenuScreen} options={{headerShown:false}} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
